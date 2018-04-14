88db090d121c486a925f49fc18fbffd60ccc8d73
Fabricio Alegria <fabr9193@gmail.com>
2018-04-14 14:19:03 +0200

    modified routes

--- flightChat/app.js
+++ flightChat/app.js
@@ -23,8 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/chat', chatRouter);
-app.use('/flight', flightRouter)
+app.use('/flight', flightRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
