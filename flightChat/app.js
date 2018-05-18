b01bafd5ee478f537db9f66263109d2aeecc36fd
Fabricio <fabricio.alegriaherrera@epitech.eu>
2018-05-18 22:39:52 +0200

    cleaning up

--- flightChat/app.js
+++ flightChat/app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+  let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var chatRouter = require('./routes/chat');
-var flightRouter = require('./routes/flight');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let chatRouter = require('./routes/chat');
+let flightRouter = require('./routes/flight');
+  let testRouter = require('./routes/test')
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/flight', flightRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
